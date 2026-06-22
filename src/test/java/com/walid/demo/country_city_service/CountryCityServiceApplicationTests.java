7491c22124faffea6ebcace36921c9e819971dcd
Walid Hamat <walidhamat@Walids-MacBook-Pro.local>
2026-06-22 12:09:15 +0200

    init: spring boot project setup

---
+++ src/test/java/com/walid/demo/country_city_service/CountryCityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walid.demo.country_city_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
