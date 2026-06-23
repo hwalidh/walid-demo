10bf1662be1ce967656bf7428e97c3a5feecd664
Walid Hamat <walidhamat@Walids-MacBook-Pro.local>
2026-06-23 14:06:52 +0200

    test: add basic unit tests for CityController

--- country-city-service/src/test/java/com/walid/demo/country_city_service/controller/CityControllerTest.java
+++ country-city-service/src/test/java/com/walid/demo/country_city_service/controller/CityControllerTest.java
@@ -16,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,7 +25,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.walid.demo.country_city_service.dto.CityDto;
